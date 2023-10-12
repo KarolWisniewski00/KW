206f1aa0128df9be92827c7ce510c0fd044bf96f
Karol <karol.wisniewski2901@gmail.com>
2023-10-12 12:04:05 +0200

    start

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,76 @@
+<x-guest-layout>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Comforter+Brush&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap');
+    </style>
+    <div class="container mx-auto my-16 bg-gray-100 rounded-3xl shadow p-4 text-center shadow">
+        <h1 class="text-4xl font-bold leadi sm:text-5xl px-8 py-4">Karol Wiśniewski
+            <span class="text-rose-500">Sklepy i strony WWW</span>
+        </h1>
+        <div class="grid grid-cols-2 gap-4 my-4">
+            <a href="tel:+48451670344" class="rounded-3xl bg-white shadow h-24 flex flex-col items-center justify-center text-xl font-extrabold my-2">
+                <i class="fa-solid fa-phone-volume"></i>Zadzwoń
+            </a>
+            <a href="mailto:karol.wisniewski2901@gmail.com" class="rounded-3xl bg-white shadow h-24 flex flex-col items-center justify-center text-xl font-extrabold my-2">
+                <i class="fa-solid fa-envelope"></i>Napisz
+            </a>
+        </div>
+        <div class="grid grid-cols-1 sm:grid-cols-3 gap-4 my-4">
+            <a href="https://baldinini-shop.pl/" class="h-full flex flex-col items-center justify-center">
+                <div class="relative h-full w-full">
+                    <div class="flex flex-col h-full items-center justify-center bg-white rounded-3xl p-5 shadow w-full">
+                        <img alt="" class="w-full h-auto" src="https://baldinini-shop.pl/wp-content/uploads/2019/11/logo.png">
+                    </div>
+                </div>
+                <h2 class="text-xl font-extrabold my-2">Obuwie</h2>
+            </a>
+            <a href="https://www.coffeesummit.pl/" class="h-full flex flex-col items-center justify-center">
+                <div class="relative my-auto">
+                    <img alt="" class="w-full h-auto rounded-3xl bg-white p-5 shadow" src="https://www.coffeesummit.pl/logo/COFFEESUMMIT-LOGO-przezroczyste-tlo.png">
+                </div>
+                <h2 class="text-xl font-extrabold my-2">Kawa</h2>
+            </a>
+            <a href="https://zlotybohomaz.pl/" class="h-full flex flex-col items-center justify-center">
+                <div class="relative h-full">
+                    <img alt="" class="w-full h-full rounded-3xl relative shadow" src="https://zlotybohomaz.pl/css/photo/hero-1.jpg">
+                    <div style="
+                    font-family: 'Comforter Brush', cursive;
+                    color: #D5AD6D;
+                    background: -webkit-linear-gradient(transparent, transparent),
+                        -webkit-linear-gradient(top, rgba(213, 173, 109, 1) 0%, rgba(213, 173, 109, 1) 26%, rgba(226, 186, 120, 1) 35%, rgba(163, 126, 67, 1) 45%, rgba(145, 112, 59, 1) 61%, rgba(213, 173, 109, 1) 100%);
+                    background: -o-linear-gradient(transparent, transparent);
+                    -webkit-background-clip: text;
+                    -webkit-text-fill-color: transparent;
+                    -webkit-text-stroke: 0.4px rgba(213, 173, 109, 1);" class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-5xl">Zloty Bohomaz</div>
+                </div>
+                <h2 class="text-xl font-extrabold my-2">Studio tatuażu</h2>
+            </a>
+            <a href="https://avantgarde-studio.pl/" class="h-full flex flex-col items-center justify-center">
+                <div class="relative h-full">
+                    <img alt="" class="w-full h-full rounded-3xl relative shadow" src="https://avantgarde-studio.pl/images/girl.png">
+                    <img alt="Logo" class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" src="https://avantgarde-studio.pl/images/logo.png">
+                </div>
+                <h2 class="text-xl font-extrabold my-2">Salon fryzjerski</h2>
+            </a>
+            <a href="https://www.zbapartments.pl/" class="h-full flex flex-col items-center justify-center">
+                <div class="relative my-auto">
+                    <img alt="" class="w-full h-auto rounded-3xl bg-white shadow" src="https://www.zbapartments.pl/image/logo.png">
+                </div>
+                <h2 class="text-xl font-extrabold my-2">Apartamenty</h2>
+            </a>
+            <a href="https://phoenixfoundiation.com/" class="h-full flex flex-col items-center justify-center">
+                <div class="relative h-full w-full">
+                    <div class="w-full rounded-3xl bg-white shadow text-white h-72 sm:h-full">.</div>
+                    <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-2xl flex flex-row items-center justify-center">
+                        <img src="https://phoenixfoundiation.com/assets/logophoenix.png" alt="logo" class="-m-3">
+                        <div class="font-2xl" style="color: #d3bdab; font-family: 'Fredoka One', cursive;">Phoenix</div>
+                    </div>
+                </div>
+                <h2 class="text-xl font-extrabold my-2">Fundacja</h2>
+            </a>
+        </div>
+    </div>
+    <div class="container mx-auto my-16 bg-gray-100 rounded-3xl shadow p-4 text-center shadow">
+        <h4 class="text-xl font-bold leadi px-8 py-4">W ofercie również administracja projektów</h4>
+    </div>
+</x-guest-layout>
